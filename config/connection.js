44177da2b696a92e9dc5f4836a2cae0c17819880
Travis Roberts <travis.roberts2@gmail.com>
2018-02-12 14:55:27 -0600

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+      connection = mysql.createConnection({
+      port: 8889,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
